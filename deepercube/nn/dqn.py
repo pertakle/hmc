65bfbafc725d0c6e1eaffaa2bae074e9b0871711
pechvi <atomychrome@gmail.com>
2024-10-22 19:56:27 +0200

    dqn

--- deepercube/nn/dqn.py
+++ deepercube/nn/dqn.py
@@ -1,14 +1,39 @@
 from deepercube.nn.her_cube_agent import HERCubeAgent
-import torch
+from deepercube.nn.network import Network
+from deepercube.utils import wrappers
 import numpy as np
+import copy
+import torch
+
 
 class DQNAgent(HERCubeAgent):
 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     def __init__(self) -> None:
         super().__init__()
+        self.model = Network(6*3*3*6, 12).to(self.device)
+        self.model.apply(wrappers.torch_init_with_xavier_and_zeros)
+        self.target_network = copy.deepcopy(self.model).to(self.device)
+        self.rng = torch.Generator()
 
-    def predict_move(self, stategals: np.ndarray, greedy: float) -> np.ndarray:
-        raise NotImplementedError
+        self.opt = torch.optim.Adam(self.model.parameters())
+        self.loss = torch.nn.MSELoss()
+
+    @wrappers.typed_torch_function(device, torch.float32, float)
+    def predict_move(self, stategoals: torch.Tensor, greedy: float) -> torch.Tensor: # type: ignore
+        self.model.eval()
+        with torch.no_grad():
+            q_values = self.model(stategoals)
+        if greedy:
+            return torch.argmax(q_values, -1)
+        return torch.randint(0, 13, size=stategoals.shape[:1], generator=self.rng)
+
     
     def train(self, episodes: np.ndarray) -> None:
         pass
+
+
+
+    def copy_weights(self) -> None:
+        self.model.load_state_dict(self.target_network.state_dict())
--- deepercube/nn/her_cube_agent.py
+++ deepercube/nn/her_cube_agent.py
@@ -5,7 +5,7 @@ class HERCubeAgent:
     def __init__(self) -> None:
         self.info = {}
 
-    def predict_move(self, stategals: np.ndarray, greedy: float) -> np.ndarray:
+    def predict_move(self, stategoals: np.ndarray, greedy: float) -> np.ndarray:
         raise NotImplementedError
     
     def train(self, episodes: np.ndarray) -> None:
